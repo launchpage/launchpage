f7ec906f9a0a27825a656ff4bb21c19cce6dcc74
Erik Moldtmann <erik@moldtmann.de>
2020-03-12 12:20:53 +0100

    first page build

---
+++ precache-manifest.a144f8e6f2895465c7beff74bdcd6749.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737938870bbe5af4bd1166e766b956a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ea445f63317acd0e59",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b514970984ec4bcdced8",
+    "url": "/static/js/2.80f61605.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.80f61605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ea445f63317acd0e59",
+    "url": "/static/js/main.62616775.chunk.js"
+  },
+  {
+    "revision": "c6722d37d7756b9e8861",
+    "url": "/static/js/runtime-main.ce9f8040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
