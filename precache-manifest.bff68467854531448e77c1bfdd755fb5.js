3f334ec86314444bee6c2e8855ff47a119a2bf8b
launchpage <erik@moldtmann.de>
2020-10-18 18:38:20 +0000

    Updates

---
+++ precache-manifest.bff68467854531448e77c1bfdd755fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a42c4ac4ea31cebbab4746ef0b40ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e12ebc6d65facf28e821",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e25b5d79e698bbac8f5d",
+    "url": "/static/js/2.02850dc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.02850dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12ebc6d65facf28e821",
+    "url": "/static/js/main.df8c750b.chunk.js"
+  },
+  {
+    "revision": "0f4aa414b7c4a202214a",
+    "url": "/static/js/runtime-main.7cb4947e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
